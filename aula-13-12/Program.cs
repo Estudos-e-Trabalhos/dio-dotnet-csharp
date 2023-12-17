883097bc328c509b76e3af7e1eb6fc4604053b0c
nicanico <nicsousan@gmail.com>
2023-12-16 21:42:42 -0300

    :tada: first commit

---
+++ aula-13-12/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using aula_13_12.Models;
+
+Console.WriteLine("Hello, World!");
+
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Nicole";
+pessoa1.Idade = 18;
+pessoa1.Apresentar();
+
+pessoa1.Nome = "Mar";
+pessoa1.Idade = 21;
+pessoa1.Apresentar();
